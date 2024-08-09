5c6d7573dd6c8f790e086ac34850bb2362371593
mcsid5397 <mpraveenkumar5397@gmail.com>
2024-08-09 22:06:27 +0530

    Commit the search algorithm program - L Search

---
+++ 01 linear-search.py
@@ -0,0 +1,19 @@
+""" 
+    Program: 01
+    Program Name: 'Linear Search'
+    Author: M. Praveen Kumar
+"""
+
+# Defining a list of numbers within the list
+num_list = [9,0,3,6,7,4,2,8]
+
+# Entering a value as search key
+search = input("Enter a number 0-10: ")
+
+# Traversing within the list to find a match
+for index in range(len(num_list)):
+    if search == num_list[index]:
+        print(f"The index of the search key is {index} and is found in the postion {index+1}")
+
+# Block off the for loop
+print(f"The value {search} couldn't be found within the list")
