fb9de65788b10e0c3054f1b651e6435fcf1abed5
mcsid5397 <mpraveenkumar5397@gmail.com>
2024-08-10 05:34:11 +0530

    Adding up function to the existing code.

--- Data-Structure/01 linear-search.py
+++ Data-Structure/01 linear-search.py
@@ -4,21 +4,26 @@
     Author: M. Praveen Kumar
 """
 
+# Defining a function  to the operation of traversing
+def find_num_postion(num_list, search):
+
+    # Traversing within the list to find a match
+    for index in range(len(num_list)):
+        if search == num_list[index]:
+            print("The index of number", search, "is", index)
+            print("The position of the number", search, "is", index + 1)
+            return  # Breaks when the conditions are satisfied and returns the index value
+
+    # If the loop is terminated without finding the number
+    if search not in num_list:
+        print("Enter a proper value")
+
+# Example usage
 # Defining a list of numbers within the list
-num_list = [9,0,3,6,7,4,2,8]
+num_list = [9, 0, 3, 6, 7, 4, 2, 8]
 """ 
     List in Python are sequence datatype,
     Reffered to as mutable datatypes i.e, it can be updated once declared/initialized.
 """
 # Entering a value as search key
 search = int(input("Enter a number 0-9: "))
-
-# Traversing within the list to find a match
-for index in range(len(num_list)):
-    if search == num_list[index]:
-        print("The index of number",search,"is",index)
-        print("The position of the number",search,"is",index+1)
-
-# Block off the for loop
-if search not in num_list:
-    print("Enter a proper value")
